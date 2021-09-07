58127b090557e9670cf9a0bd3741f957074835ac
notajunster@gmail.com <notajunster@gmail.com>
2021-09-07 14:21:45 -0400

    bad

--- Snake/Pong.cpp
+++ Snake/Pong.cpp
@@ -2,14 +2,36 @@
 #include "Graphics.h"
 #include "Pong.h"
 
+double getRadiansPi()
+{
+	srand(time(NULL));
+	int toreturn = (rand() % 200);
+	switch (toreturn)
+	{
+	case 50:
+		getRadiansPi();
+		break;
+	case 100:
+		getRadiansPi();
+		break;
+	case 150:
+		getRadiansPi();
+		break;
+	case 200:
+		getRadiansPi();
+		break;
+	default:
+		return (double) toreturn / 100;
+	}
+}
+
 void Pong::Load()
 {
 	frame = 0;
-	ballSpeed = 500.0;
-	measure = 35.0;
+	ballSpeed = 100.0;
 	xPosition = 500.0;
 	yPosition = 500.0;
-	positiveX = true;
+	radiansPi = getRadiansPi();
 }
 void Pong::Unload()
 {
@@ -17,7 +39,129 @@ void Pong::Unload()
 
 void Pong::Update(double timeTotal, double timeDelta)
 {
-	getNewPos();
+	// Ball physics
+	// TODO: Handler for corrners
+	{
+		double xDelta;
+		double yDelta;
+		double tempX;
+		double tempY;
+		double measure;
+
+		switch ((int) (radiansPi * 2))
+		{
+		case 0:
+
+			measure = -tan(radiansPi * M_PI);
+
+			xDelta = 800 - xPosition;
+			yDelta = xDelta * measure;
+			tempX = xPosition + xDelta;
+			tempY = yPosition + yDelta;
+			wallX = tempX;
+			wallY = tempY;
+
+			if (tempY <= 0)
+			{
+				wallY = 0;
+				wallX = tempX + (tempY / measure);
+			}
+
+			if (tempY >= 600)
+			{
+				wallY = 600;
+				wallX = tempX - (tempY / measure);
+			}
+
+			break;
+
+		case 1:
+
+			measure = tan(radiansPi * M_PI);
+
+			xDelta = xPosition;
+			yDelta = xDelta * measure;
+			tempX = xPosition - xDelta;
+			tempY = yPosition + yDelta;
+			wallX = tempX;
+			wallY = tempY;
+
+			if (tempY <= 0)
+			{
+				wallY = 0;
+				wallX = tempX + (tempY / measure);
+			}
+
+			if (tempY >= 600)
+			{
+				wallY = 600;
+				wallX = tempX - (tempY / measure);
+			}
+			break;
+
+		case 2:
+
+			measure = tan(radiansPi * M_PI);
+
+			xDelta = xPosition;
+			yDelta = xDelta * measure;
+			tempX = xPosition + xDelta;
+			tempY = yPosition + yDelta;
+			wallX = tempX;
+			wallY = tempY;
+
+			if (tempY <= 0)
+			{
+				wallY = 0;
+				wallX = tempX + (tempY / measure);
+			}
+
+			if (tempY >= 600)
+			{
+				wallY = 600;
+				wallX = tempX - (tempY / measure);
+			}
+
+			break;
+
+		case 3:
+
+			measure = tan(radiansPi * M_PI);
+
+			xDelta = 800 - xPosition;
+			yDelta = xDelta * measure;
+			tempX = xPosition + xDelta;
+			tempY = yPosition - yDelta;
+			wallX = tempX;
+			wallY = tempY;
+
+			if (tempY <= 0)
+			{
+				wallY = 0;
+				wallX = tempX + (tempY / measure);
+			}
+
+			if (tempY >= 600)
+			{
+				wallY = 600;
+				wallX = tempX - (tempY / measure);
+			}
+
+			break;
+
+		default:
+			break;
+		}
+
+		long double delta = sqrt(
+			(pow((wallX - xPosition), 2) + 
+			(pow((wallY - yPosition), 2))));
+		long double distanceRatio = (ballSpeed * timeDelta) / delta;
+		xPosition = ((xPosition * (1 - distanceRatio)) + (wallX * (distanceRatio)));
+		yPosition = ((yPosition * (1 - distanceRatio)) + (wallY * (distanceRatio)));
+
+		if ()
+	}
 }
 
 void Pong::Render()
@@ -39,136 +183,3 @@ void Pong::Render()
 
 	gfx->FillCircle(xPosition, yPosition, 10, 1.0f, 1.0f, 1.0f, 1.0f);
 }
-
-// Ball physics
-// TODO: Handler for corrners
-int Pong::getNewPos()
-{
-	// Case +
-	if (positiveX)
-	{
-		OutputDebugStringW(L"PosX\n");
-
-		double tempnewxPos = xPosition +
-			sqrt((pow(ballSpeed, 2.0) / (1 + pow(measure, 2.0))));
-		double tempnewyPos = ((tempnewxPos - xPosition) / measure) + yPosition;
-
-		if (tempnewxPos >= 800)
-		{
-			OutputDebugStringW(L"PosX > 800\n");
-
-			//Perpendicular lines
-			double xDelta = 800 - xPosition;
-			double yDelta = xDelta * measure;
-			double wallX = xPosition + xDelta;
-			double wallY = yPosition + yDelta;
-			double totalDelta = sqrt(pow(xDelta, 2) + pow(yDelta, 2));
-
-			double tempnewxPos = wallX +
-				sqrt((pow(ballSpeed - totalDelta, 2.0) / (1 + pow(-measure, 2.0))));
-			double tempnewyPos = ((tempnewxPos - wallX) / -measure) + wallY;
-
-			xPosition = tempnewxPos;
-			yPosition = tempnewyPos;
-			
-			measure = -1.0 / measure;
-			positiveX = !positiveX;
-
-			return 0;
-		}
-		if (tempnewyPos <= 0 || tempnewyPos >= 600)
-		{
-			OutputDebugStringW(L"PosX > 800, Y\n");
-
-			double yDelta = ((yPosition) * (xPosition < 300)) + ((600 - xPosition) * (xPosition > 300)); //We are no longer Yandev
-			double xDelta = yDelta / measure;
-			double wallX = xPosition + xDelta;
-			double wallY = yPosition + yDelta;
-			double totalDelta = sqrt(pow(xDelta, 2) + pow(yDelta, 2));
-
-			double tempnewxPos = wallX +
-				sqrt((pow(ballSpeed - totalDelta, 2.0) / (1 + pow(-measure, 2.0))));
-			double tempnewyPos = ((tempnewxPos - wallX) / -measure) + wallY;
-
-			xPosition = tempnewxPos;
-			yPosition = tempnewyPos;
-
-			measure = -1.0 / measure;
-
-			return 0;
-		}
-
-		else
-		{
-			xPosition = tempnewxPos;
-			yPosition = tempnewyPos;
-
-			return 0;
-		}
-
-	}
-
-	// Case -
-	else
-	{
-		OutputDebugStringW(L"NotPosX\n");
-
-		double tempnewxPos = xPosition -
-			sqrt((pow(ballSpeed, 2.0) / (1 + pow(measure, 2.0))));
-		double tempnewyPos = ((tempnewxPos - xPosition) / measure) + yPosition;
-
-		if (tempnewxPos <= 0)
-		{
-			OutputDebugStringW(L"PosX < 0\n");
-
-			//Perpendicular lines
-			double xDelta = xPosition;
-			double yDelta = xDelta * measure;
-			double wallX = xPosition + xDelta;
-			double wallY = yPosition + yDelta;
-			double totalDelta = sqrt(pow(xDelta, 2) + pow(yDelta, 2));
-
-			double tempnewxPos = wallX +
-				sqrt((pow(ballSpeed - totalDelta, 2.0) / (1 + pow(measure, 2.0))));
-			double tempnewyPos = ((tempnewxPos - wallX) / measure) + wallY;
-
-			xPosition = tempnewxPos;
-			yPosition = tempnewyPos;
-
-			measure = -1.0 / measure;
-			positiveX = !positiveX;
-
-			return 0;
-		}
-		if (tempnewyPos <= 0 || tempnewyPos >= 600)
-		{
-			OutputDebugStringW(L"PosX < 0, Y\n");
-
-			double yDelta = ((yPosition) * (xPosition < 300)) + ((600 - xPosition) * (xPosition > 300)); //We are no longer Yandev
-			double xDelta = yDelta / measure;
-			double wallX = xPosition + xDelta;
-			double wallY = yPosition + yDelta;
-			double totalDelta = sqrt(pow(xDelta, 2) + pow(yDelta, 2));
-
-			double tempnewxPos = wallX +
-				sqrt((pow(ballSpeed - totalDelta, 2.0) / (1 + pow(-measure, 2.0))));
-			double tempnewyPos = ((tempnewxPos - wallX) / -measure) + wallY;
-
-			xPosition = tempnewxPos;
-			yPosition = tempnewyPos;
-
-			measure = -1.0 / measure;
-
-			return 0;
-		}
-
-		else
-		{
-			xPosition = tempnewxPos;
-			yPosition = tempnewyPos;
-
-			return 0;
-		}
-	}
-	
-};
--- Snake/Pong.h
+++ Snake/Pong.h
@@ -1,16 +1,18 @@
 #pragma once
 
+#define _USE_MATH_DEFINES
+
 #include "GameLevel.h"
 #include <math.h>
+#include <ctime>
 
 class Pong : public GameLevel
 {
-	double measure;
+	double radiansPi;
 	double xPosition, yPosition;
-	double newxPosition, newyPosition;
+	double wallX, wallY;
 	double ballSpeed;
 	int frame;
-	bool positiveX;
 
 public:
 
